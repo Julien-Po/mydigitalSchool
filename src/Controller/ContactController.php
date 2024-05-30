46363a997b2ed878ffb1bfaf9fbae62b981967d4
Julien <poirierjulien30@gmail.com>
2024-05-30 14:43:35 +0200

    Envoi de mail dispo

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,6 +6,8 @@ use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,7 +15,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request, EntityManagerInterface $manager): Response
+    public function index(Request $request, EntityManagerInterface $manager, MailerInterface $mailer): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
@@ -25,6 +27,26 @@ class ContactController extends AbstractController
 
             $manager->persist($contact);
             $manager->flush();
+
+            $email = (new TemplatedEmail())
+            ->from($contact->getEmail())
+            ->to('poirierjulien30@gmail.com')
+            ->subject($contact->getSubject())
+            ->htmlTemplate('email/email.html.twig')
+
+            // change locale used in the template, e.g. to match user's locale
+            ->locale('de')
+        
+            // pass variables (name => value) to the template
+            ->context([
+                'contact' => $contact
+            ])
+        ;
+
+        $mailer->send($email);
+
+        // ...
+    
         }
 
         return $this->render('contact/index.html.twig', [
